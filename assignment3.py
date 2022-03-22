961721c3e636ab6a719cbc3f0c0f74e27d6786e1
cin kang <100551563+Kirsin3rd@users.noreply.github.com>
2022-03-22 10:52:18 +0800

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -18,6 +18,36 @@ l=food_list[-4:-1]
 print(k)
 print(l)
 del food_tuple
+vegetables='lettuce','broccoli','spinach','shallot'
+fruits='avocado','passion fruit','tangerine','kiwi'
+fruit_vegetable=vegetables+fruits
+print(fruit_vegetable)
+num=len(fruit_vegetable)
+print(num)
+food_tuple=fruit_vegetable+('needle murshroom',"yoghourt",)
+print(food_tuple)
+(a,b,c,d,e,f,g,h,i,j)=food_tuple
+print(i,j)
+food_list=list(food_tuple)
+i=food_tuple[4:6]
+print(i)
+j=food_list[1:8:3]
+print(j)
+k=food_list[:4]
+l=food_list[-4:-1]
+print(k)
+print(l)
+del food_tuple
+try :
+    print(food_tuple)
+except NameError:
+    print('tuple has been deleted')
+
+asian_countries = 'India','China','Singapore','Thailand','Indonesia'
+m='Finland'in asian_countries
+n='India'in asian_countries
+print(m)
+print(n)
 
 asian_countries = 'India','China','Singapore','Thailand','Indonesia'
 m='Finland'in asian_countries
